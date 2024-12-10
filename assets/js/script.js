980f79093e6885581b649de6d174a3816d8c4c05
Zeinab Abu El-soud <zeinababoalsoud09@gmail.com>
2024-12-10 15:00:13 +0200

    frist-commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// object
+var array1 =["zeinab", 'lawan','amira'];
+// 
+var object1 ={
+    name:'zeinab',
+    age:24,
+    lastname:"Abu El-soud",
+    fullname:function(){
+        return this.name +" "+ this.age;
+    }
+    
+    // key = name >> age >> country>> object named key
+};
+console.log(object1.fullname())
+// query brackets : colon 
+
+// var object2 =new Object();
+// object2.name="farha";
+// object2.age=25;
+
+// console.log(object1['name']);
+// console.log(object1.name);
+// console.log(object1);
+
+// var question1 ={
+//     qtitle:"what is html ?",
+//     qnumber:1,
+// };
+
