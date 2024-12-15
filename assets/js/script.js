035769ccc66bd1531d9ae4e25b7c8090f2b5af73
Zeinababoalsoud <zeinababoalsoud09@gmail.com>
2024-12-16 00:25:14 +0200

    ...

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ function checkScreenSize() {
   
     if (window.matchMedia("(max-width: 992px)").matches) {
 
-        button.innerText= " Join ";
-        button.style.width ='80px';
+        button.innerText= "+ Join ";
+        button.style.width ='100px';
         backgroundImg.style.backgroundImage ='none';
 
     } 
@@ -14,6 +14,20 @@ function checkScreenSize() {
 
 window.addEventListener('resize', checkScreenSize);
 window.addEventListener('load', checkScreenSize);
+// Slider
+const openSidebarBtn = document.getElementById('openSidebar');
+const closeSidebarBtn = document.getElementById('closeSidebar');
+const sidebar = document.getElementById('sidebar');
+
+
+openSidebarBtn.addEventListener('click', () => {
+  sidebar.style.left = '0';
+});
+
+closeSidebarBtn.addEventListener('click', () => {
+  sidebar.style.left= '-100%';
+});
+
 
 
 
